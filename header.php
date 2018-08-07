67cc65bcda18e81da7c6c2d51b9349c64d4b28c5
anil <aniljangidaj6@gmail.com>
2018-08-08 00:37:45 +0530

    First commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+
+<body> 
+	<header>
+			<nav>
+				<div class="main-wrapper">
+					<ul>
+						<li><a href="index.php">HOME</a>
+						</li>
+					</ul>
+						<div class="nav-login">
+							<form>
+								<input type="text" name="uid" placeholder="Username/Email" >		
+				     			<input type="password" name="pwd" placeholder="Password" >						
+								<button type="submit" name="submit">Login</button>	
+							</form>
+							<a href="signup.php">Signup</a>
+						</div>
+				</div>
+			</nav> 
+		
+	</header>
---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+$dbServername="localhost";
+$dbUsername="root";
+$dbPasswrd="";
+$dbName="loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPasswrd, $dbName);
+?>
+
